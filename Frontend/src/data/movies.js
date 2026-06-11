f09df390295a5bdbd359b92280b843a24a300313
Jeet Chauhan <spgtjeet123@gmail.com>
2026-06-11 15:49:18 +0530

    Adding Data

---
+++ Frontend/src/data/movies.js
@@ -0,0 +1,118 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Stranger Things",
+    description:
+      "When a young boy vanishes, a small town uncovers a mystery involving secret experiments and supernatural forces.",
+    image: "https://picsum.photos/id/237/1920/1080",
+    rating: 8.7,
+    genre: "Sci-Fi",
+    year: 2016,
+    runtime: "50m",
+  },
+
+  {
+    id: 2,
+    title: "The Witcher",
+    description:
+      "Geralt of Rivia journeys toward his destiny in a turbulent world.",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMTU3NjE5NzYtYTYyNS00MDVmLWIwYjgtMmYwYWIxZDYyNzU2XkEyXkFqcGdeQXVyNzQzNzQxNzI@._V1_SY1000_SX677_AL_.jpg",
+    rating: 8.2,
+    genre: "Fantasy",
+    year: 2019,
+    runtime: "60m",
+  },
+
+  {
+    id: 3,
+    title: "Wednesday",
+    description:
+      "Wednesday Addams investigates a murder spree while navigating life at Nevermore Academy.",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BZmIxMjA3ZDItNWZlMC00NDE0LWI0MTEtY2U5ZTg0MzFiZjAwL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMjUxODE0MDY@._V1_SY1000_SX677_AL_.jpg",
+    rating: 8.1,
+    genre: "Mystery",
+    year: 2022,
+    runtime: "45m",
+  },
+
+  {
+    id: 4,
+    title: "Dark",
+    description:
+      "A family saga with a supernatural twist set in a German town.",
+    image: "https://picsum.photos/id/1005/1920/1080",
+    rating: 8.8,
+    genre: "Thriller",
+    year: 2017,
+    runtime: "55m",
+  },
+
+  {
+    id: 5,
+    title: "Breaking Bad",
+    description:
+      "A chemistry teacher diagnosed with cancer turns to manufacturing methamphetamine.",
+    image: "https://picsum.photos/id/1011/1920/1080",
+    rating: 9.5,
+    genre: "Crime",
+    year: 2008,
+    runtime: "47m",
+  },
+
+  {
+    id: 6,
+    title: "Money Heist",
+    description:
+      "A criminal mastermind plans the biggest heist in recorded history.",
+    image: "https://picsum.photos/id/1025/1920/1080",
+    rating: 8.3,
+    genre: "Crime",
+    year: 2017,
+    runtime: "50m",
+  },
+  {
+    id: 7,
+    title: "Peaky Blinders",
+    description: "A notorious gang in Birmingham rises to power.",
+    image: "https://picsum.photos/id/1040/1920/1080",
+    rating: 8.8,
+    genre: "Crime",
+    year: 2013,
+    runtime: "60m",
+  },
+
+  {
+    id: 8,
+    title: "Loki",
+    description: "The God of Mischief alters timelines.",
+    image: "https://picsum.photos/id/1041/1920/1080",
+    rating: 8.2,
+    genre: "Fantasy",
+    year: 2021,
+    runtime: "52m",
+  },
+
+  {
+    id: 9,
+    title: "The Last of Us",
+    description: "A smuggler escorts a young girl across America.",
+    image: "https://picsum.photos/id/1042/1920/1080",
+    rating: 8.9,
+    genre: "Drama",
+    year: 2023,
+    runtime: "58m",
+  },
+
+  {
+    id: 10,
+    title: "House of the Dragon",
+    description: "The Targaryen civil war begins.",
+    image: "https://picsum.photos/id/1043/1920/1080",
+    rating: 8.5,
+    genre: "Fantasy",
+    year: 2022,
+    runtime: "60m",
+  },
+];
